b7284ba600530c99f6165e4195fd3bd5796670bb
adjohnston <adam.johnston3679@gmail.com>
2014-02-06 08:31:35 +0000

    progress

--- assets/js/tcpl/responsive-ajax-slider.js
+++ assets/js/tcpl/responsive-ajax-slider.js
@@ -15,34 +15,59 @@
         $.fn.tcplSlider = function (options) {
 
             var defaults = $.extend({
+                hasPaging: false,
+
                 ajaxUrl: undefined,
                 ajaxData: undefined,
 
                 startSlide: 0,
+                maxSlides:  5,
 
-                $slides: $('.carousel__slide'),
-
+                $slider:  $('.carousel__slides'),
+                $slides:  $('.carousel__slide'),
                 $prevBtn: $('.carousel__prev'),
                 $nextBtn: $('.carousel__next'),
 
-                prevBtnTxt: '<',
-                nextBtnTxt: '>',
+                $pager: $('.carousel__paging'),
+                page:   'carousel__page',
 
-                onLoad: function () { }
+                prevBtnTxt: '<',
+                nextBtnTxt: '>'
             }, options),
 
+            i = 0,
+
+            //  set the width of the slide, update on resize
             slideWidth = defaults.$slides.width(),
 
+            //  current slide that is in view
             currentSlide = defaults.startSlide,
+
+            //  total number of slides, update on new slide added
             totalSlides = defaults.$slides.length,
 
             init = function () {
+                //  check whether to add paging
+                if (defaults.hasPaging && defaults.maxSlides) {
+
+                    for (i; i < totalSlides; i++) {
+
+                        defaults.$pager.append($('<li />').attr({
+                            'data-slide': i,
+                            class:   defaults.page
+                        }).text(i + 1));
+
+                    }
+
+                    i = 0;
+                }
+
                 //  give buttons text
                 defaults.$prevBtn.text(defaults.prevBtnTxt);
                 defaults.$nextBtn.text(defaults.nextBtnTxt);
 
                 //  get total width of slider based on slider width x number of slides
-                $('.carousel__slides').width(slideWidth * totalSlides);
+                defaults.$slider.width(slideWidth * totalSlides);
 
                 //  inline set slide width
                 defaults.$slides.width(slideWidth);
@@ -53,87 +78,95 @@
 
             checkBtnState = function () {
                 //  check whether the current slide is at start or end
-                var hidePrev = (currentSlide === defaults.startSlide) ? true : false,
-                    hideNext = (currentSlide === (totalSlides - 1)) ? true : false;
-
-                if (hidePrev) {
-                    defaults.$prevBtn.hide();
-                } else {
-                    defaults.$prevBtn.show();
-                }
-
-                if (hideNext && !(defaults.ajaxUrl && defaults.ajaxData)) {
-                    defaults.$nextBtn.hide();
-                } else {
-                    defaults.$nextBtn.show();
-                }
+                return {
+                    isStart: (currentSlide === defaults.startSlide) ? true : false,
+                    isEnd:   (currentSlide === (totalSlides - 1)) ? true : false
+                };
             },
 
-            loadSlide = function (callback) {
-                var $lastSlide = defaults.$slides.last();
-
+            getSlide = function (callback) {
                 $.ajax({
                     url: defaults.ajaxUrl,
                     data: defaults.ajaxData,
 
                     success: function (data) {
-                        $('.carousel__slides').append(data);
-                        var blah = 1;
+                        defaults.$slider.append(data);
+
+                        data.page++;
                     }
                 });
-
-                //$lastSlide.append(html);
-
-                if (typeof callback === 'function') {
-
-                    return callback();
-
-                }
-
             },
 
-            prevSlide = function () {
-                if (!checkBtnState()) {
+            //  refactor prev and next into one and allow moving by amounts
+            moveSlide = function (moveBy) {
+                //  value must exist
+                if (moveBy) {
+                    moveBy = moveBy * currentSlide;
 
-                    $(defaults.$slides[0]).css({
-                        transform: 'translateX(' + slideWidth * (currentSlide - 1) + ')',
-                        marginLeft: slideWidth * (currentSlide - 1)
+                    defaults.$slides.first().css({
+                        marginLeft: (moveBy) ? slideWidth * moveBy : slideWidth
                     });
 
-                    currentSlide--;
+                    //  increment
+
+                } else {
+
+                    throw new Error('moveToSlide was not given a moveBy param');
 
                 }
-
-                checkBtnState();
-            },
-
-            nextSlide = function () {
-                if (!checkBtnState()) {
-
-                    if ((currentSlide === totalSlides - 1) && (defaults.ajaxUrl && defaults.ajaxData)) {
-                        loadSlide();
-
-                        totalSlides++;
-                    }
-
-                    $(defaults.$slides[0]).css({
-                        transform: 'translateX(' + slideWidth * (currentSlide - 1) + ')',
-                        marginLeft: -slideWidth * (currentSlide + 1)
-                    });
-
-                    currentSlide++;
-
-                }
-
-                checkBtnState();
             };
 
-            defaults.$prevBtn.on('click', prevSlide);
-            defaults.$nextBtn.on('click', nextSlide);
+            // prevSlide = function () {
+            //     if (!checkBtnState()) {
 
+            //         $(defaults.$slides[0]).css({
+            //             transform: 'translateX(' + slideWidth * (currentSlide - 1) + ')',
+            //             marginLeft: slideWidth * (currentSlide - 1)
+            //         });
+
+            //         currentSlide--;
+
+            //     }
+
+            //     checkBtnState();
+            // },
+
+            // nextSlide = function () {
+            //     if (!checkBtnState() && currentSlide < defaults.maxSlides) {
+
+            //         if ((currentSlide === totalSlides - 1) && (defaults.ajaxUrl && defaults.ajaxData)) {
+            //             getSlide();
+
+            //             totalSlides++;
+            //         }
+
+            //         $(defaults.$slides[0]).css({
+            //             transform: 'translateX(' + slideWidth * (currentSlide - 1) + ')',
+            //             marginLeft: -slideWidth * (currentSlide + 1)
+            //         });
+
+            //         currentSlide++;
+
+            //     }
+
+            //     checkBtnState();
+            // };
+
+            //  event listeners
+            defaults.$prevBtn.on('click', function () {
+                moveSlide(1);
+            });
+
+            defaults.$nextBtn.on('click', function () {
+                moveSlide(-1)
+            });
+
+            //  run on each instance of tcplSlider()
             return this.each(function () {
+                //  init slider
                 init();
 
+                //  used for chaining
                 return this;
             });
 
